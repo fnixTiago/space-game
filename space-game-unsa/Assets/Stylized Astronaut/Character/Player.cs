1409a222d7b132b87f20d3d008a842abe86530bf
fnixTiago <israel.only23@gmail.com>
2021-12-27 09:20:53 -0500

    fake

--- space-game-unsa/Assets/Stylized Astronaut/Character/Player.cs
+++ space-game-unsa/Assets/Stylized Astronaut/Character/Player.cs
@@ -1,47 +1,35 @@
 using UnityEngine;
 using System.Collections;
 
-public class Player : MonoBehaviour
-{
-    public Transform PuntoInicio;
-    public GameObject player;
-    private Animator anim;
-    private CharacterController controller;
+public class Player : MonoBehaviour {
 
-    public float speed = 600.0f;
-    public float turnSpeed = 400.0f;
-    private Vector3 moveDirection = Vector3.zero;
-    public float gravity = 20.0f;
+		private Animator anim;
+		private CharacterController controller;
 
-    void Start()
-    {
-        this.transform.position = PuntoInicio.position;
-        controller = player.GetComponent<CharacterController>();
-        anim = player.GetComponentInChildren<Animator>();
-    }
-    public void Reiniciar() { 
-        this.transform.position = PuntoInicio.position;
-    }
+		public float speed = 600.0f;
+		public float turnSpeed = 400.0f;
+		private Vector3 moveDirection = Vector3.zero;
+		public float gravity = 20.0f;
 
-    void Update()
-    {
-        if (Input.GetKey("w"))
-        {
-            anim.SetInteger("AnimationPar", 1);
-        }
-        else
-        {
-            anim.SetInteger("AnimationPar", 0);
-        }
+		void Start () {
+			controller = GetComponent <CharacterController>();
+			anim = gameObject.GetComponentInChildren<Animator>();
+		}
 
-        if (controller.isGrounded)
-        {
-            moveDirection = transform.forward * Input.GetAxis("Vertical") * speed;
-        }
+		void Update (){
+			if (Input.GetKey ("w")) {
+				anim.SetInteger ("AnimationPar", 1);
+			}  else {
+				anim.SetInteger ("AnimationPar", 0);
+			}
 
-        float turn = Input.GetAxis("Horizontal");
-        transform.Rotate(0, turn * turnSpeed * Time.deltaTime, 0);
-        controller.Move(moveDirection * Time.deltaTime);
-        moveDirection.y -= gravity * Time.deltaTime;
-    }
+			if(controller.isGrounded){
+				moveDirection = transform.forward * Input.GetAxis("Vertical") * speed;
+			}
+
+			float turn = Input.GetAxis("Horizontal");
+			transform.Rotate(0, turn * turnSpeed * Time.deltaTime, 0);
+			controller.Move(moveDirection * Time.deltaTime);
+			moveDirection.y -= gravity * Time.deltaTime;
+		}
 }
